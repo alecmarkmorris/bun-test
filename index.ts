d1f3367551ebad0025a427ff25ebb38e7536698b
alecmarkmorris <Alecmarkmorris@gmail.com>
2023-10-18 22:34:10 -0700

    Editing Web server

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const url = new URL(req.url)
+
+        if(url.pathname === '/ALGORITHM'){
+            const body = figlet.textSync("Take your Panties Off");
+            return new Response(body)
+        }
+
+        if(url.pathname === '/'){
+            const body = figlet.textSync("Welcome To The Server");
+            return new Response(body)
+        }
+
+        if(url.pathname === '/ALLI'){
+            var message = figlet.textSync("I Love You");
+            return new Response(message)
+        }
+
+        return new Response("404 NOT FOUND")
+    }
+})
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
